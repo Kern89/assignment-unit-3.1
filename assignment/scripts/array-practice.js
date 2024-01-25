c08c46237264e539cc92b453b66221c273c82a7b
Kenneth Grina <grina.kenneth@gmail.com>
2024-01-24 20:48:52 -0600

    answered Q3&4a-b

--- assignment/scripts/array-practice.js
+++ assignment/scripts/array-practice.js
@@ -36,18 +36,20 @@ console.log('First animal is', firstAnimal);
 //      of the second item from the `animalArray`.
 //      Be sure to console.log this new variable to make sure your code
 //      worked as expected.
-
+let secondAnimal = animalArray[1];
+console.log(secondAnimal);
 
 // 3.b. TODO: Create a variable `lastAnimal` and assign it the value of
 //      the "last" item in `animalArray`, using its array index.
 //      You'll need to console.log `animalArray` and `lastAnimal` to make
 //      sure that your code does what you want. (Never trust your code until
 //      you have proof that it works!)
-
+let lastAnimal = animalArray[3];
+console.log(lastAnimal);
 // 3.c. (STRETCH) TODO: Refactor 3.b to use the `animalArray` variable's .length 
 //       property, rather than the exact (hardcoded) index number of the last item.
-
-
+let lastAnimal2 = animalArray[animalArray.length - 1];
+console.log(lastAnimal2);
 
 // Adding & Removing Array Items
 console.log('--- 4. Adding and removing array items ---');
@@ -61,7 +63,9 @@ console.log('Added an animal to end,', animalArray);
 //      a dessert that you love.
 //      Similar to above, add the `dessert` to the end of your `favoriteFoods`
 //      array. (How can you be 100% certain this worked? 🤔)
-
+let dessert = 'cinnamon bears';
+favoriteFoods.push(dessert);
+console.log('Added a dessert to my favorite foods: ', favoriteFoods);
 
 // Example: Add to the beginning of an array using the Array.unshift method.
 let animalToUnshift = 'walrus';
@@ -71,7 +75,9 @@ console.log(`Added an animal to beginning: ${animalArray}`);
 // 4.b. TODO: Create a variable `entree` and assign it a string value of
 //      an entree that you enjoy.
 //      Add the `entree` to the beginning of your `favoriteFoods` array.
-
+let entree = 'swedish meatballs';
+favoriteFoods.unshift(entree);
+console.log(favoriteFoods);
 
 // Example: Remove the last animal by using the Array.pop method.
 let removedAnimal = animalArray.pop();
